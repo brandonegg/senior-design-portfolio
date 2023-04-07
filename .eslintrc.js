8c42eb76b5c3b0b7f6018bedb947b625a2987989
Brandon Egger <brandonegger64@gmail.com>
2023-04-07 01:30:10 -0500

    add eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,41 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
   extends: ["@remix-run/eslint-config", "@remix-run/eslint-config/node"],
+  "overrides": [
+    {
+      "extends": [
+        "plugin:@typescript-eslint/recommended-requiring-type-checking"
+      ],
+      "files": ["*.ts", "*.tsx"],
+      "parserOptions": {
+        "project": "tsconfig.json"
+      }
+    }
+  ],
+  "ignorePatterns": ["*.config.js"],
+  "plugins": ["@typescript-eslint"],
+  "rules": {
+    "semi": "error",
+    "eol-last": "error",
+    "no-trailing-spaces": "error",
+    "@typescript-eslint/consistent-type-imports": "warn",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "import/order": [
+      "error",
+      { "newlines-between": "always", "alphabetize": { "order": "asc" } }
+    ],
+    "sort-imports": [
+      "error",
+      { "ignoreDeclarationSort": true, "ignoreCase": true }
+    ],
+    "functional/no-return-void": "off",
+    "@typescript-eslint/no-misused-promises": [
+      2,
+      {
+        "checksVoidReturn": {
+          "attributes": false
+        }
+      }
+    ]
+  }
 };
