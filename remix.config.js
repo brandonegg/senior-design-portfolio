e6e8408fc5ec87c79d47614c19c219d3152e5571
Brandon Egger <brandonegger64@gmail.com>
2023-04-28 00:45:41 -0500

    remove future features

--- remix.config.js
+++ remix.config.js
@@ -4,10 +4,4 @@ module.exports = {
   future: {
     unstable_tailwind: true,
   },
-  future: {
-    v2_errorBoundary: true,
-    v2_meta: true,
-    v2_normalizeFormMethod: true,
-    v2_routeConvention: true,
-  },
 };
